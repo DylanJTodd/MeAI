2f5ab85a9c8b3b71a7b5cfa8fe7db183103499c0
Dylan Todd <dylan.todd04@gmail.com>
2025-06-05 10:51:09 -0400

    chore: edit dataset to include less negative examples and finetune for less epochs

--- train.py
+++ train.py
@@ -77,7 +77,7 @@ if __name__ == "__main__":
         output_dir="./results",
         per_device_train_batch_size=1,
         per_device_eval_batch_size=4,
-        num_train_epochs=5,
+        num_train_epochs=3,
         save_strategy="epoch",
         learning_rate=2e-4,
         weight_decay=0.01,
